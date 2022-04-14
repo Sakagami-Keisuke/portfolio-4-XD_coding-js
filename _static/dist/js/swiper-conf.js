ff2fd50e7bc82fc964415757cdc1452bf8a00940
Macintosh <macintosh@makkintosshunoMacBook-Pro.local>
2022-04-14 10:16:05 +0900

    pc view works section fix

--- _static/dist/js/swiper-conf.js
+++ _static/dist/js/swiper-conf.js
@@ -1,8 +1,9 @@
 const swiper = new Swiper('.swiper', {
-	slidesPerView: 4,     // 一度に表示させるスライドの数
-	spaceBetween: 5,     // スライド間の間隔px
+	slidesPerView: 4,      // 一度に表示させるスライドの数
+	spaceBetween: 155,     // スライド間の間隔px
+	centeredSlides: true,  //1枚目のスライド中央配置
 	autoplay: {
-		delay: 5000, // スライドの間隔（単位はミリ秒）
+		delay: 5000,      // スライドの間隔（単位はミリ秒）
 	  },
 	  loop: true,
 
